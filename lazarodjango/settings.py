24a1bd9f95bdcbe6d3fe90a91853d57521da83d3
laguimarfh <laguimarfh@gmail.com>
2022-11-03 20:00:30 -0400

    start django

--- lazarodjango/settings.py
+++ lazarodjango/settings.py
@@ -21,10 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%%2q(snumvq^m#hc2y7l-+cx54$x$ixp#kv918-q-7u7r$^%jf'
+# SECRET_KEY = 'django-insecure-%%2q(snumvq^m#hc2y7l-+cx54$x$ixp#kv918-q-7u7r$^%jf'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-%%2q(snumvq^m#hc2y7l-+cx54$x$ixp#kv918-q-7u7r$^%jf')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['*']
 
